f85c99051377442dfae2632cbd84aa08b8ffaa50
mdsaddamvol <mdsaddamvoltas@gmail.com>
2020-12-23 09:13:27 -0600

    smoothing drag and drop

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -25,36 +25,15 @@ const Card = ({ id, index, img, data, modalOpen, moveCard }) => {
 			if (!ref.current) {
 				return;
 			}
+
 			const dragIndex = item.index;
 			const hoverIndex = index;
 			// Don't replace items with themselves
 			if (dragIndex === hoverIndex) {
 				return;
 			}
-			// Determine rectangle on screen
-			const hoverBoundingRect = ref.current?.getBoundingClientRect();
-			// Get vertical middle
-			const hoverMiddleX =
-				(hoverBoundingRect.right - hoverBoundingRect.left) / 2;
-			// Determine mouse position
-			const clientOffset = monitor.getClientOffset();
-			// Get pixels to the top
-			const hoverClientX = clientOffset.x - hoverBoundingRect.left;
-
-			console.log(hoverMiddleX);
-			// Only perform the move when the mouse has crossed half of the items height
-			// When dragging downwards, only move when the cursor is below 50%
-			// When dragging upwards, only move when the cursor is above 50%
-			// Dragging downwards
-			if (dragIndex < hoverIndex && hoverClientX > hoverMiddleX) {
-				return;
-			}
-			// Dragging upwards
-			if (dragIndex > hoverIndex && hoverClientX > hoverMiddleX) {
-				return;
-			}
-			// Time to actually perform the action
 			moveCard(dragIndex, hoverIndex);
+
 			// Note: we're mutating the monitor item here!
 			// Generally it's better to avoid mutations,
 			// but it's good here for the sake of performance
@@ -68,7 +47,7 @@ const Card = ({ id, index, img, data, modalOpen, moveCard }) => {
 			isDragging: monitor.isDragging(),
 		}),
 	});
-	const opacity = isDragging ? 0 : 1;
+	const opacity = isDragging ? 1 : 1;
 	drag(drop(ref));
 
 	return (
